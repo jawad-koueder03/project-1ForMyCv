55703b8ee0a4c9907a5a220987d9fb8f7121b818
jawad-koueder03 <jawad.kou.03@gmail.com>
2026-09-22 22:23:50 +0300

    fix path

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/project-1formycv">
+    <BrowserRouter basename="/project-1ForMyCv">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
